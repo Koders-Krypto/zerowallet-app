699e9303523e58f6fa402362169f9945e0510827
Rohan Reddy <rohanreddy888@gmail.com>
2024-08-28 01:28:44 +0530

    Minor Tweaks

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className=" bottom-0 left-0 md:flex items-center justify-center w-full text-sm md:text-base text-center text-white">
+    <footer className=" bottom-0 left-0 md:flex items-center justify-center w-full text-sm md:text-base text-center px-2 text-white">
       <div className="flex flex-row flex-wrap items-center justify-center w-full gap-2 pb-3">
         Made with 🤍 at ETHOnline 2024 - Powered by{" "}
         <Image
