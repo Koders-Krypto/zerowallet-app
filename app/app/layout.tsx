1695f6c7aa3b1b4dfd260ebba10948be35fabe56
Rohan Reddy <rohanreddy888@gmail.com>
2024-08-29 20:54:14 +0530

    Added Tx UI & UI Tweaks

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -13,7 +13,7 @@ export default function AppLayout({
       <div className="flex flex-row gap-0 items-start justify-start px-5 py-4 md:py-4 md:px-4 w-full min-h-screen">
         <TxPopup />
         <Sidebar />
-        <div className="flex flex-col gap-6 md:gap-8 justify-start items-start w-full h-[97vh] md:border md:border-l-0 border-accent md:p-5">
+        <div className="flex flex-col gap-6 md:gap-8 justify-start items-start w-full h-[97vh] flex-grow md:border md:border-l-0 border-accent md:p-5">
           <Topbar />
           {children}
         </div>
